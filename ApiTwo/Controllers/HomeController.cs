0181650fab8067be02eb33363f6f0eff85f500fc
phanhoangnam <phanhoangnamst@gmail.com>
2021-10-14 23:14:31 +0700

    add identity server 4

---
+++ ApiTwo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace ApiTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("/")]
+        public async Task<IActionResult> Index()
+        {
+            //retrieve access token
+            var serverClient = _httpClientFactory.CreateClient();
+            var discoveryDocument = await serverClient.GetDiscoveryDocumentAsync("https://localhost:44356/");
+            var tokenRespone = await serverClient.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest
+                {
+                    Address = discoveryDocument.TokenEndpoint,
+                    ClientId = "client_id",
+                    ClientSecret = "client_secret",
+                    Scope = "ApiOne"
+                });
+
+            // retrieve secret data
+            var apiClient = _httpClientFactory.CreateClient();
+            apiClient.SetBearerToken(tokenRespone.AccessToken);
+            var respone = await apiClient.GetAsync("https://localhost:44334/secret");
+            var content = respone.Content.ReadAsStringAsync();
+
+            return Ok(new
+            {
+                access_token = tokenRespone.AccessToken,
+                message = content,
+            });
+        }
+    }
+}

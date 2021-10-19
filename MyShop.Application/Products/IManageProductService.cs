1fe1af0baa9432e2deafade54418e14aff179669
phanhoangnam <phanhoangnamst@gmail.com>
2021-10-19 11:31:28 +0700

    add product view model, product service

---
+++ MyShop.Application/Products/IManageProductService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.ViewModels.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Application.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+        Task<int> Delete(int productId);
+        Task<ProductViewModel> GetById(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+    }
+}
---
+++ MyShop.ViewModels/Products/ProductCreateRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.ViewModels.Products
+{
+    public class ProductCreateRequest
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
